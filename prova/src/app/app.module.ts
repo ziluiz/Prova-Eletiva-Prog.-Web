67db044f105d5721f689b19614665964cfda0b5e
ziluiz <luiz021205@gmail.com>
2022-12-13 08:50:57 -0300

    segundo

--- prova/src/app/app.module.ts
+++ prova/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowdataComponent } from './showdata/showdata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowdataComponent
   ],
   imports: [
     BrowserModule,
